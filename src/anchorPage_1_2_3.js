e2783bdaa26ecaf198dda6aee8903ffb2fa3d58b
unknown <adeniji@ris-development.de>
2018-03-12 22:09:49 +0100

    Bugfixes and added Smoother Scrolling Experience

--- src/anchorPage_1_2_3.js
+++ src/anchorPage_1_2_3.js
@@ -1,9 +1,12 @@
 /**
- *AnchorPageJS Version 1.1.1
+ *AnchorPageJS Version 1.2.3
  *Author: HerrMutig
  *License: MIT
  */
+
+Fixed Weird sliding animations
 (function ($) {
+    var anchorPageFunctionCalls = 0;
     $.fn.anchorPage = function (options) {
         /***PROPERTIES***/
         var settings = $.extend({
@@ -15,12 +18,14 @@
         }, options);
 
         /***GLOBAL VARIABLES***/
-
+        
         var posObjArr = [];
         var currentPos;
         var maxPos;
         var minPos;
         var lockAnimation;
+        
+        //Desktop
         var movedByWheel;
         var lastMouseScroll;
 
@@ -29,14 +34,23 @@
         var touchScreenEndYPos;
         var movedByTouch;
         var touchDirection;
-
-
-        /***Initialization***/
+        
         posObjArr = $(this).length > 0 ? $(this) : null;
+        /***Valid Input Checking***/
+        if(posObjArr === null)
+            return;
+        if(anchorPageFunctionCalls > 1)         //Kills function if called more than once
+            return;
+        
+        anchorPageFunctionCalls++;
+        
+        /***Initialization***/
         maxPos = posObjArr.length - 1;
         minPos = 0;
         lockAnimation = false;
-
+        
+        
+        
         /***Functions***/
         function animateOnComplete() {
             setTimeout(function () {
@@ -46,8 +60,20 @@
                 settings.waitingtime);
         }
 
-        function animateOnStart() {
-            lockAnimation = true;
+        function animateOnStart(e) {
+            while($(this).queue().length >  1) //if more than one animation will get played. Delete whole queue but not the one which is running atm
+            {
+                $(this).stop();
+            }
+              
+
+            //Locks further animation for the time of settings.waitingtime
+           lockAnimation = true;
+        }
+        function animationOnRun()
+        {      
+           
+
         }
 
         function animateWindow(position) {
@@ -55,6 +81,7 @@
                 scrollTop: position
             }, {
                 start: animateOnStart,
+                step: animationOnRun,
                 complete: animateOnComplete,
                 duration: settings.duration,
                 easing: settings.easing
@@ -62,29 +89,35 @@
         }
 
         function allowMoving(event) {
-            // console.log("MAXPOS: " + maxPos + " currentPos: " + getCurrentPosition() + "ScrollDirection: " + getScrollDirection(event));
+          
+       //     console.log("PosObjArr: " + ($(posObjArr[getCurrentPosition()]).offset().top + settings.endMargin));
             if (movedByWheel) {
                 if (minPos === getCurrentPosition() && getScrollDirection(event) === "up" || maxPos === getCurrentPosition() && getScrollDirection(event) === "down") {
                     return true;
+                    
                 }
                 //property start margin
-                if ($(window).scrollTop() > posObjArr[maxPos].offsetTop + settings.endMargin && getScrollDirection(event) === "up") {
-                    return true;
+                if ($(window).scrollTop() > Math.round($(posObjArr[maxPos]).offset().top + settings.endMargin) && getScrollDirection(event) === "up") {
+                   return true;
+                          
                 }
-                if ($(window).scrollTop() < posObjArr[minPos].offsetTop - settings.startMargin && getScrollDirection(event) === "down") {
-                    return true;
+                if ($(window).scrollTop() < Math.round($(posObjArr[minPos]).offset().top - settings.startMargin) && getScrollDirection(event) === "down") {
+                   return true;
+              
                 }
 
             }
+            console.log("WINDOW: " + $(window).scrollTop());
+            console.log("POS:" + ($(posObjArr[getCurrentPosition()]).offset().top - settings.startMargin));
             if (movedByTouch) {       
                 if (minPos === getCurrentPosition() && getTouchDirection() === "down" || maxPos === getCurrentPosition() && getTouchDirection(event) === "up") {
                     return true;
                 }
                 //property start margin
-                if ($(window).scrollTop() < posObjArr[minPos].offsetTop - settings.startMargin && getTouchDirection(event) === "up") {
+                if ($(window).scrollTop() < Math.round($(posObjArr[minPos]).offset().top - settings.startMargin) && getTouchDirection(event) === "up") {
                     return true;
                 }
-                if ($(window).scrollTop() > posObjArr[maxPos].offsetTop + settings.endMargin && getTouchDirection(event) === "down") {
+                if ($(window).scrollTop() > Math.round($(posObjArr[maxPos]).offset().top + settings.endMargin) && getTouchDirection(event) === "down") {
                     return true;
                 }
             }
@@ -94,38 +127,51 @@
 
 
         function scrollToAnchor(restriction) {
+            movedByWheel = false;
+            movedByTouch = false;
             if (lockAnimation === true)
                 return;
+            
             var scrollToAnchor = getCurrentPosition();
             if (restriction === "next") {
-                if ($(window).scrollTop() < posObjArr[minPos].offsetTop) {
+                if ($(window).scrollTop() < Math.round($(posObjArr[minPos]).offset().top)) {
                     scrollToAnchor = getCurrentPosition() - 1;
                 }
-                animateWindow(posObjArr[scrollToAnchor + 1].offsetTop);
+                animateWindow(Math.round($(posObjArr[scrollToAnchor + 1]).offset().top));
             } else if (restriction === "prev") {
 
-                if ($(window).scrollTop() > posObjArr[maxPos].offsetTop) {
+                if ($(window).scrollTop() > Math.round($(posObjArr[maxPos]).offset().top)) {
                     scrollToAnchor = getCurrentPosition() + 1;
                 }
 
-                animateWindow(posObjArr[scrollToAnchor - 1].offsetTop);
+                animateWindow(Math.round($(posObjArr[scrollToAnchor - 1]).offset().top));
             }
             return false;
         }
+        
 
-        function getCurrentPosition() {
+        function setCurrentPosition()
+        {
             currentPos = 0;
-            for (var i = 0; i < posObjArr.length; i++) {
-                if ($(window).scrollTop() >= posObjArr[i].offsetTop) {
+            for (var i = 0; i < posObjArr.length; i++) 
+            {
+                if ($(window).scrollTop() >= Math.round($(posObjArr[i]).offset().top)) 
+                {
                     currentPos = i;
                 }
             }
+           // console.log("POSITION: "+ currentPos);
+        }
+
+        function getCurrentPosition() {
+            if(currentPos === undefined)
+                setCurrentPosition();
             return currentPos;
         }
         //Desktop
         function getScrollDirection(event) {
-            if (event.originalEvent.deltaY === undefined) {
-                return lastMouseScroll;
+            if (event.originalEvent === undefined) {
+                return lastMouseScroll === undefined ? -1 : lastMouseScroll;
             } else if (event.originalEvent.deltaY < 0) {
                 lastMouseScroll = "up";
                 return lastMouseScroll;
@@ -135,26 +181,19 @@
             }
         }
 
-        function blockExceededMouseScrolling(event) {
+        function blockExceededMovementScrolling(event) {
+        //console.log("Window: " + $(window).scrollTop(3299));
             //If no animation is played but a scroll happens it checks if scroll is valid
-            if (!allowMoving(event) && lockAnimation === false) {
-                if (getCurrentPosition() === minPos) {
-                    $(window).scrollTop(posObjArr[minPos].offsetTop - settings.startMargin);
-                } else if (getCurrentPosition() === maxPos) {
-                    $(window).scrollTop(posObjArr[maxPos].offsetTop + settings.endMargin);
-                }
-            }
-        }
-
-        //Mobile
-
-        function blockExceededTouchScrolling(event) {
-            if (!allowMoving(event) && lockAnimation === false) {
-                if (getCurrentPosition() === minPos) {
-                    $(window).scrollTop(posObjArr[minPos].offsetTop - settings.startMargin);
-                } else if (getCurrentPosition() === maxPos) {
-                    $(window).scrollTop(posObjArr[maxPos].offsetTop + settings.endMargin);
-                }
+            if (!allowMoving(event) && lockAnimation === false) 
+            {          
+                    if(getCurrentPosition() === minPos)
+                    {
+                         scrollToAnchor("next"); //Smooth Scrolling
+                    }
+                    else if (getCurrentPosition() === maxPos)
+                    {                       
+                        scrollToAnchor("prev"); //Smooth Scrolling
+                    }
             }
         }
 
@@ -178,11 +217,14 @@
         /***Event Functions***/
         //Desktop
         function EventScrollToSection(event) {
+           
             movedByWheel = true;
             movedByTouch = false;
+            getScrollDirection(event);
+            setCurrentPosition();
             if (allowMoving(event)) {
                 return;
-            }
+            }           
             event.preventDefault();
             if (getScrollDirection(event) === "up") {
                 scrollToAnchor("prev");
@@ -193,22 +235,25 @@
 
 
         function EventScrollBlock(event) {
-            blockExceededMouseScrolling(event);
+            if(movedByTouch || movedByWheel)
+                blockExceededMovementScrolling(event);
         }
 
 
 
         //Mobile
-        function EventTouchStart(event) {
+        function EventTouchStart(event) 
+        {
             touchScreenStartYPos = event.touches[0].screenY;
             movedByWheel = false;
             movedByTouch = true;
+            setCurrentPosition();
 
         }
 
         function EventTouchMove(event) {
             if (!allowMoving(event)) {
-               event.preventDefault();
+               //event.preventDefault();
             }
 
             touchScreenEndYPos = event.touches[0].screenY;
@@ -232,15 +277,19 @@
 
 
         /***Event Bindings***/
-        $(window).on("wheel", EventScrollToSection);
-        $(window).on("scroll", EventScrollBlock);
-
+        
+        
         //JQuery doesn't support options on EventListener
+        window.addEventListener("scroll", EventScrollBlock, {passive: true});
+        $(window).on("wheel", EventScrollToSection);
+        $(window).on("resize ", function(){});
         window.addEventListener("touchstart", EventTouchStart);
-        window.addEventListener("touchmove", EventTouchMove, {
-            passive: false, cancelable: true, cancelBubble: true
-        });
+        window.addEventListener("touchmove", EventTouchMove, {passive: false, cancelable: true, cancelBubble: true});
         window.addEventListener("touchend", EventTouchToSection);
+      
+        
 
     };
+    
+    
 }(jQuery));
